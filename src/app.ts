ebe63bd8671439c02d03b35a0f4d908a9d5f0b61
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-07-17 20:10:17 +0800

    feat(auth); protected all routes with auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { errorHandler } from "./shared/middleware/error.middleware.js";
+import { requireAuth } from "./shared//middleware/auth.middleware.js";
 import { config } from "./shared/config/env.js";
 import { openapiSpec } from "./shared/config/openapi.js";
 import checklistRoutes from "./checklist/checklist.routes.js";
@@ -15,9 +16,9 @@ if (!config.isProduction) {
   app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
 }
 
-app.use("/api/v1/checklist", checklistRoutes);
-app.use("/api/v1/user", userProfileRoutes);
-app.use("/api/v1/bike", bikeProfileRoutes);
+app.use("/api/v1/checklist", requireAuth, checklistRoutes);
+app.use("/api/v1/user", requireAuth, userProfileRoutes);
+app.use("/api/v1/bike", requireAuth, bikeProfileRoutes);
 app.use(errorHandler);
 
 export default app;
